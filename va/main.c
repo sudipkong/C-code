6490672cb776a0b5cb4e30c3b241c34f29e22671
Sudip <sudipkong44@gmail.com>
2023-05-02 00:50:20 +0530

    new code test

---
+++ va/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+
+
+
+
+
+int main()
+{
+    int a = 5;
+    int b = 2;
+
+
+    float z = a/(float)b;           // convert to float and divide
+
+    printf("%f\n", z);
+
+
+
+
+
+    return  0;
+}
